ec3c592fd32c6512052e0689e5b5acaf81f5bd8e
grenadierman <bert@trioptimum.com>
2015-02-21 11:33:21 -0800

    debug no map testfile

---
+++ src/debugNOMAP.py
@@ -0,0 +1,15 @@
+import pygame
+
+from player import Player
+from context import Context
+from constants import *
+
+print '\n#################### STARTING BASIC TEST ####################'
+print '\n######## Testing Success/Combat ########\n'
+
+print 'Testing dice success diGiTALQU33F'
+result = Context.test
+print ' Test results: %s\n' % result
+print '\n######################## END OF TEST ########################\n'
+
+crap = raw_input()
